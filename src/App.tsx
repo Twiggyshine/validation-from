61b6f124ac53984ada424874f3e74b691b508a8d
Olga <olga_bezrukova@bk.ru>
2025-04-01 20:06:27 +0300

    Initial commit for registration form

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import './App.css'
 import SignInForm from './components/form.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 
 
 function App() {
- 
-
   return (
-    <>
-      <SignInForm />
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<SignInForm />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
